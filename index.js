2a077993d38d600bc8c88911858b6fb5742d57d8
Siddharth Ranka <siddharthranka91@gmail.com>
2026-04-16 10:17:14 +0530

    websocket connection added

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const http = require('http');
+const { Server } = require('socket.io');
+
 dotenv.config();
 console.log('Redis URL:', process.env.UPSTASH_REDIS_URL);
-require('./src/workers/notificationWorker');
+
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
 app.use(express.json());
-const notificationRoutes=require('./src/routes/notificationRoutes');
-app.use('/api/notifications',notificationRoutes);
+
+require('./src/workers/notificationWorker')(io); //passing socket instance to worker 
+
+const notificationRoutes = require('./src/routes/notificationRoutes');
+app.use('/api/notifications', notificationRoutes);
+
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=>console.log('mongoDB connected'))
-    .catch((err)=>console.log("mongoDB error :",err));
+  .then(() => console.log('mongoDB connected'))
+  .catch((err) => console.log("mongoDB error :", err));
 
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
 
-app.get('/',(req,res)=>{
-    res.json({message:"notification engine running"});
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server running on port ${process.env.PORT}`);
+httpServer.listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
 });
--- src/workers/notificationWorker.js
+++ src/workers/notificationWorker.js
@@ -1,33 +1,47 @@
-const {Worker} = require('bullmq');
+const { Worker } = require('bullmq');
 require('dotenv').config();
-const {sendEmail}=require('../services/emailServices');
-const {sendSMS}=require('../services/smsService');
-const  connection ={
-    url:process.env.UPSTASH_REDIS_URL
+const { sendEmail } = require('../services/emailServices');
+const { sendSMS } = require('../services/smsService');
+
+const connection = {
+  url: process.env.UPSTASH_REDIS_URL
 };
-const worker = new Worker('notifications',async(job)=>{
-    console.log('processing job:',job.id);
-const  {recipient,message,type}=job.data;
-if(type=='email'){
-    await sendEmail(recipient,message);
-}else if(type=='sms'){
-    await sendSMS(recipient,message);
-}
-},{
-connection,
-attempts:3,
-backoff:{
-    type:'exponential',
-    delay:1000
-}
-});
 
-worker.on('completed', (job) => {
-  console.log(`Job ${job.id} completed successfully`);
-});
+module.exports = (io) => {
+  const worker = new Worker('notifications', async (job) => {
+    console.log('processing job:', job.id);
+    const { recipient, message, type } = job.data;
 
+    if (type === 'email') {
+      await sendEmail(recipient, message);
+    } else if (type === 'sms') {
+      await sendSMS(recipient, message);
+    }
+ 
+    //io.emit->pushed data to all conneted browsers instantly
+    io.emit('notification', { //this sends to frontend 
+      status: 'delivered',
+      type,
+      recipient,
+      message
+    });
 
-worker.on('failed',(job,err)=>{
-    console.log(`${job.id} failed:`,err.message);
-})
-module.exports=worker;
+  }, {
+    connection,
+    attempts: 3,
+    backoff: {
+      type: 'exponential',
+      delay: 1000
+    }
+  });
+
+  worker.on('completed', (job) => {
+    console.log(`Job ${job.id} completed successfully`);
+  });
+
+  worker.on('failed', (job, err) => {
+    console.log(`${job.id} failed:`, err.message);
+  });
+
+  return worker;
+};
